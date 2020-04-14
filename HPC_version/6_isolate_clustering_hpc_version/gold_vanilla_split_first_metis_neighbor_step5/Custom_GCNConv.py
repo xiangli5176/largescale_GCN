9379eb31b718ed18734bba40cb31636dea8ddee3
XiangLi <xiangli13257@gmail.com>
2020-04-14 15:32:35 -0400

    add split first

---
+++ HPC_version/6_isolate_clustering_hpc_version/gold_vanilla_split_first_metis_neighbor_step5/Custom_GCNConv.py
@@ -0,0 +1,94 @@
+import math
+import random
+import torch
+from torch_geometric.nn import MessagePassing
+from torch.nn import Parameter
+
+from torch_scatter import scatter_add
+import torch.nn.functional as F
+from torch_geometric.utils import add_remaining_self_loops
+
+from torch_geometric.nn import GCNConv
+
+
+############################################
+########## Customized Neuro Nets ###########
+############################################
+
+### ====================== Establish a GCN based model ========================
+class ListModule(torch.nn.Module):
+    """
+    Abstract list layer class.
+    """
+    def __init__(self, *args):
+        """
+        Module initializing.
+        """
+        super(ListModule, self).__init__()
+        idx = 0
+        for module in args:
+            self.add_module(str(idx), module)
+            idx += 1
+
+    def __getitem__(self, idx):
+        """
+        Getting the indexed layer.
+        """
+        if idx < 0 or idx >= len(self._modules):
+            raise IndexError('index {} is out of range'.format(idx))
+        it = iter(self._modules.values())
+        for i in range(idx):
+            next(it)
+        return next(it)
+
+    def __iter__(self):
+        """
+        Iterating on the layers.
+        """
+        return iter(self._modules.values())
+
+    def __len__(self):
+        """
+        Number of layers.
+        """
+        return len(self._modules)
+
+
+class Net(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, input_layers = [16, 16], dropout=0.3):
+        """
+        input layers: list of integers
+        dropout: probability of droping out 
+        """
+        super(Net, self).__init__()
+        
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.input_layers = input_layers
+        self.dropout = dropout
+        self.setup_layers()
+
+    def setup_layers(self):
+        """
+        Creating the layes based on the args.
+        """
+        self.layers = []
+        self.input_layers = [self.in_channels] + self.input_layers + [self.out_channels]
+        for i, _ in enumerate(self.input_layers[:-1]):
+            self.layers.append(GCNConv(self.input_layers[i],self.input_layers[i+1]))
+        self.layers = ListModule(*self.layers)
+
+    # change the dropout positions: 
+    def forward(self, edge_index, features):
+        if len(self.layers) > 1:
+            for i in range(len(self.layers)-1):
+                features = F.relu(self.layers[i](features, edge_index))
+#                 if i>0:
+                features = F.dropout(features, p = self.dropout, training = self.training)
+                    
+            features = self.layers[len(self.layers)-1](features, edge_index)
+        else:
+            features = self.layers[0](features, edge_index)    # for a single layer case
+
+        predictions = F.log_softmax(features, dim=1)
+        return predictions
