c815bf5f8c1296408a512f6bafa333824604e10f
xiangli5176 <xiangli13257@gmail.com>
2020-08-18 21:12:47 -0400

    update results

---
+++ HPC_version_GCN/9_GraphSage_geometric_hpc_version/gold_Step32_Sage_enhance_multilabel_optimal_epoch/Custom_GCNConv.py
@@ -0,0 +1,109 @@
+import math
+import random
+import torch
+from torch_geometric.nn import MessagePassing
+from torch.nn import Parameter
+
+from torch_scatter import scatter_add
+import torch.nn.functional as F
+from torch_geometric.utils import add_remaining_self_loops
+
+from sagegcn_conv_enhance import SAGEConv
+
+
+############################################
+########## Customized Neuro Nets ###########
+############################################
+
+### ====================== Establish a GCN based model ========================
+class ListModule(torch.nn.Module):
+    """
+    Abstract list layer class.
+    """
+    def __init__(self, *args):
+        """
+        Module initializing.
+        """
+        super(ListModule, self).__init__()
+        idx = 0
+        for module in args:
+            self.add_module(str(idx), module)
+            idx += 1
+
+    def __getitem__(self, idx):
+        """
+        Getting the indexed layer.
+        """
+        if idx < 0 or idx >= len(self._modules):
+            raise IndexError('index {} is out of range'.format(idx))
+        it = iter(self._modules.values())
+        for i in range(idx):
+            next(it)
+        return next(it)
+
+    def __iter__(self):
+        """
+        Iterating on the layers.
+        """
+        return iter(self._modules.values())
+
+    def __len__(self):
+        """
+        Number of layers.
+        """
+        return len(self._modules)
+
+
+class Net(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, input_layers = [16, 16], dropout=0.3, improved = False, diag_lambda = -1):
+        """
+        input layers: list of integers
+        dropout: probability of droping out 
+        """
+        super(Net, self).__init__()
+        
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.input_layers = input_layers
+        self.dropout = dropout
+        self.improved = improved 
+        self.diag_lambda = diag_lambda
+        self.setup_layers()
+
+    def setup_layers(self):
+        """
+        Creating the layes based on the args.
+        """
+        self.layers = []
+        self.input_layers = [self.in_channels] + self.input_layers + [self.out_channels]
+        
+        for i in range(len(self.input_layers)-1):
+            self.layers.append(SAGEConv(self.input_layers[i], self.input_layers[i+1], 
+                                       improved = self.improved, diag_lambda = self.diag_lambda, dropout = self.dropout))
+        self.layers = ListModule(*self.layers)
+        
+        self.bn_layers = []
+        for i in range(1, len(self.input_layers)):
+            self.bn_layers.append(torch.nn.BatchNorm1d(self.input_layers[i]))
+        self.bn_layers = ListModule(*self.bn_layers)
+
+    # change the dropout positions: 
+    def forward(self, edge_index, features):
+        if len(self.layers) > 1:
+            for i in range(len(self.layers)-1):
+                features = self.layers[i](features, edge_index, dropout_training=self.training)
+#                 features = F.dropout(features, p = self.dropout, training = self.training)
+                features = self.bn_layers[i](features)
+                features = F.relu(features)
+            
+            features = self.layers[len(self.layers)-1](features, edge_index, dropout_training=self.training)
+        else:
+            features = self.layers[0](features, edge_index, dropout_training=self.training)    # for a single layer case
+        
+        predictions = features
+        # just use the linear layer output, since we are using the cross-entropy loss
+        # just pay attention to the test part, change the predictions
+        
+#         predictions = F.log_softmax(features, dim=1)
+        # if using the nll loss, then we need this log_softmax layer
+        return predictions
---
+++ HPC_version_GCN/9_GraphSage_geometric_hpc_version/gold_Step32_Sage_enhance_multilabel_optimal_epoch/formal_exec.py
@@ -0,0 +1,107 @@
+
+from multi_exec_code import *
+
+
+if __name__ == '__main__':
+    # current_path = os.environ.get('PBS_O_WORKDIR').split('/')
+    # data_name = current_path[-3]
+    # test_folder_name = 'train_10%_full_neigh/'
+    # intermediate_data_folder = './'
+    # image_data_path = intermediate_data_folder + 'results/' + data_name + '/' + test_folder_name
+    
+    # # this is the parts we divide the graph
+    # origin_train_batch_num = 75
+    # round_num = 1
+    # train_batch_num = round_num * origin_train_batch_num
+
+    # GCN_layer = [128]
+    # net_layer_num = len(GCN_layer) + 1
+    # # for non-optimization: hop_layer_num == net_layer_num
+    # hop_layer_num = net_layer_num
+
+    # tune_param_name = 'learning_rate' 
+
+    # tune_val_label_list = [4]
+    # tune_val_list = [10**(-label) for label in tune_val_label_list]
+
+    # trainer_list = list(range(7))
+
+    # from GraphSaint_dataset import print_data_info, Flickr, Yelp, PPI_large, Amazon, Reddit
+    # remote_data_root = '~/GCN/Datasets/GraphSaint/'
+    # data_class = eval(data_name)
+    # dataset = data_class(root = remote_data_root + data_name)
+    # data = dataset[0]
+    # print_data_info(data, dataset)
+
+
+    # pc version test on small version of PPI
+    data_name = 'PPI_small'
+    test_folder_name = 'Graph_Sage/'
+    intermediate_data_folder = './tuning_lr_template/'
+    image_data_path = intermediate_data_folder + 'results/' + data_name + '/' + test_folder_name
+
+    # this is the parts we divide the graph
+    train_batch_num = 8
+
+    GCN_layer = [256]
+    net_layer_num = len(GCN_layer) + 1
+    # for non-optimization: hop_layer_num == net_layer_num
+    hop_layer_num = net_layer_num
+
+    tune_param_name = 'learning_rate'
+    tune_val_label_list = [3, 4]
+    tune_val_list = [10**(-label) for label in tune_val_label_list]
+
+    trainer_list = list(range(2))
+    res_model_epoch_list = list(range(10, 201, 10))
+
+
+    from GraphSaint_dataset import print_data_info, Flickr, Yelp, PPI_large, Amazon, Reddit, PPI_small
+    remote_data_root = '/home/xiangli/projects/tmpdata/GCN/GraphSaint/'
+    class_data = eval(data_name)
+    dataset = class_data(root = remote_data_root + data_name)
+    print('number of data', len(dataset))
+    data = dataset[0]
+    print_data_info(data, dataset)
+
+
+    # need to make sure the train and validation has the same number of batches
+    step0_generate_clustering_machine(data, dataset, intermediate_data_folder, train_batch_num = train_batch_num, validation_batch_num = train_batch_num,
+                                    validation_ratio = 0.12, test_ratio = 0.22)
+
+    step1_generate_train_batch(intermediate_data_folder, sample_num = 10, \
+                            batch_range = (0, train_batch_num), 
+                            info_folder = 'info_train_batch/' )
+
+    step2_generate_batch_whole_graph(intermediate_data_folder, info_folder = 'info_whole/')
+
+
+    # investigate train
+    for tune_val_label, tune_val in zip(tune_val_label_list, tune_val_list):
+        for tainer_id in trainer_list:
+            step31_run_investigation_train_batch(intermediate_data_folder, tune_param_name, tune_val_label, tune_val, train_batch_num, net_layer_num, GCN_layer,
+                            trainer_id = tainer_id, dropout = 0.1, lr = 0.001, weight_decay = 0.1, mini_epoch_num = 10, improved = True, diag_lambda = -1,
+                                                epoch_num = 200, output_period = 10)
+            
+
+    # investigate validation
+    for tune_val_label, tune_val in zip(tune_val_label_list, tune_val_list):
+        for trainer_id in trainer_list:
+            step41_run_investigation_validation_batch(image_data_path, intermediate_data_folder, tune_param_name, tune_val_label, tune_val, train_batch_num, net_layer_num,
+                        trainer_id = trainer_id, model_epoch = res_model_epoch_list)
+
+
+    for tune_val_label, tune_val in zip(tune_val_label_list, tune_val_list):
+        step51_run_investigation_summarize_whole(data_name, image_data_path, intermediate_data_folder, tune_param_name, tune_val_label, tune_val,
+                                    train_batch_num, net_layer_num, trainer_list, res_model_epoch_list)
+
+    
+    # investigate test
+    for tune_val_label, tune_val in zip(tune_val_label_list, tune_val_list):
+        for trainer_id in trainer_list:
+            execute_test_tuning(image_data_path, intermediate_data_folder, res_model_epoch_list, 
+                                    tune_param_name, tune_val_label, tune_val, trainer_id = trainer_id)
+
+
+    step50_run_tune_summarize_whole(data_name, image_data_path, intermediate_data_folder, tune_param_name, tune_val_label_list, tune_val_list, \
+                                train_batch_num, net_layer_num, trainer_list)                                    
